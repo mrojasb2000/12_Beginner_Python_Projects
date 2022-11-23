6d205c9e0d5cf125d62e41c8710731769fa4289f
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-23 19:16:19 -0300

    feat: added random madlibs

--- madlibs.py
+++ madlibs.py
@@ -7,12 +7,19 @@ youtuber = "Mavro" # some string variable
 # print("subscribe to {}".format(youtuber))
 # print(f"subscribe to {youtuber}")
 
-adj = input("Adjective: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous person: ")
+# adj = input("Adjective: ")
+# verb1 = input("Verb: ")
+# verb2 = input("Verb: ")
+# famous_person = input("Famous person: ")
+# 
+# madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+# I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+# 
+# print(madlib)
 
-madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
-I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+from sample_madlibs import hp, code, zombie, hungergames
+import random
 
-print(madlib)
+if __name__ == "__main__":
+    m = random.choice([hp, code, zombie, hungergames])
+    m.madlib()
