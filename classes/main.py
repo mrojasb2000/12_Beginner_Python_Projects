e171149964139eadbdef6c5fde6f0dbb075afbbe
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-04 14:48:25 -0300

    chore: added default value params

--- classes/main.py
+++ classes/main.py
@@ -1,6 +1,6 @@
 class Item:
     
-    def __init__(self, name, price, quantity):
+    def __init__(self, name, price, quantity = 0):
         print(f"An instance created: {name}")
         self.name = name
         self.price = price
@@ -9,7 +9,7 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
-item1 = Item("Phone", 100, 5)
+item1 = Item("Phone", 100)
 print(item1.calculate_total_price())
 
 item2 = Item("Laptop", 1000, 3)
