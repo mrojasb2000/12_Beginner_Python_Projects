4891a6bcf46eb0cd7462087c797034d681bca6b4
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-04 13:42:50 -0300

    chore: added params and attibutes class

--- classes/main.py
+++ classes/main.py
@@ -1,16 +1,19 @@
 class Item:
     
-    def __init__(self, name):
+    def __init__(self, name, price, quantity):
         print(f"An instance created: {name}")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self):
         return self.price * self.quantity
 
-item1 = Item("Phone")
-#print(item1.calculate_total_price())
+item1 = Item("Phone", 100, 5)
+print(item1.calculate_total_price())
 
-item2 = Item("Laptop")
-#print(item2.calculate_total_price())
+item2 = Item("Laptop", 1000, 3)
+print(item2.calculate_total_price())
 
 # Get type
 #print(type(item1))
