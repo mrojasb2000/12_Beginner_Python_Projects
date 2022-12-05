a328f85e60752a1e022b36b78be455a77f6cda99
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-04 22:51:43 -0300

    refactor: first unit test

--- classes/test_main.py
+++ classes/test_main.py
@@ -1,7 +1,8 @@
 import pytest
 from main import Item
 
-def test_answer():
+def test_should_return_total_price():
     item1 = Item("Phone", 100, 5)
-
-    assert item1.calculate_total_price() == 500
+    expected = 500
+    actual = item1.calculate_total_price()
+    assert actual == expected
