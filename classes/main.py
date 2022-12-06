dd5458780b3a4ed074be75b65e8b36acf04f74f7
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-05 21:06:59 -0300

    chore: added vlidations arguments

--- classes/main.py
+++ classes/main.py
@@ -1,7 +1,11 @@
 class Item:
     
-    def __init__(self, name, price, quantity = 0):
-        print(f"An instance created: {name}")
+    def __init__(self, name: str, price: float, quantity: int = 0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greate or equal to than zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greate or equal than zero!"
+         
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -9,10 +13,9 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
-item1 = Item("Phone", 100)
+item1 = Item("Phone", 100, 1)
 print(item1.calculate_total_price())
 
 item2 = Item("Laptop", 1000, 3)
 item2.has_numpad = False
 print(item2.calculate_total_price())
-
